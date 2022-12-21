f5d1bf9cad1a419816f2819735fb9b069459d7c5
Anika222 <121105319+Anika222@users.noreply.github.com>
2022-12-21 15:55:44 +0600

    File upload

---
+++ eshop/dashboard/footer.php
@@ -0,0 +1,11 @@
+<?php
+echo '<!-- footer -->
+<!-- developed by mb_mishu -->
+<footer class="footer-area">
+    <div class="container-fluid">
+        <div class="row">
+            
+        </div>
+    </div>
+
+</footer>';
